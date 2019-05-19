9b8e26722d2e8489f060f1b74b537992b37b6c43
weigang <weigang@weigangdeMacBook-Pro.local>
2019-05-19 22:32:49 +0800

    4

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    mkfn()
+    mkfn('.js_mk ul li')
     //模块动画
-    function mkfn() {
-        var ali = $('.js_mk ul li');
+    function mkfn(id) {
+        var ali = $(id);
         // 移入
         ali.on('mouseenter', function () {
             $(this).find('div.text').animate({
-                left: 0
+                'left': 0
             }, 200)
         });
         // 移出
@@ -15,9 +15,34 @@ $(document).ready(function () {
             setTimeout(() => {
                 $(this).find('div.text').removeClass('rotatetext')
                 $(this).find('div.text').stop().animate({
-                    left: -($(this).find('img').width()) + 'px',
+                    'left': -($(this).width()) + 'px',
                 }, 0)
             }, 200);
         });
     }
+    mkfn1('.js_cgal ul li')
+    //模块动画
+    function mkfn1(id) {
+        var ali = $(id);
+        // 移入
+        ali.on('mouseenter', function () {
+            $(this).find('div.text').animate({
+                'bottom': 0
+            }, 500)
+        });
+        // 移出
+        ali.on('mouseleave', function () {
+            $(this).find('div.text').stop().animate({
+                'bottom': -($(this).width()) + 'px',
+            }, 500)
+
+        });
+    }
+    //登录弹出框
+    $('.js_login').on('mouseenter',function(){
+        $(this).find('div.list').show()
+    })
+    $('.js_login').on('mouseleave',function(){
+        $(this).find('div.list').hide()
+    })
 })
